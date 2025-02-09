44b987b1517f1483751f5567812f18bfe2043504
pynindaeyo <thisispyn@gmail.com>
2025-02-09 18:24:03 +0700

    18.23
    finished all page, only design left

--- script.js
+++ script.js
@@ -8,13 +8,4 @@ let revealed = 0;
 
 function showMessage(index) {
     let messageBox = document.getElementById("hint-message");
-    messageBox.innerText = messages[index];
-    messageBox.style.display = "block"; // Ensure the message is visible
-
-    revealed++;
-    
-    // Show the Next button after all messages are revealed
-    if (revealed === messages.length) {
-        document.getElementById("next-button").style.display = "block";
-    }
-}
+    messageBo
